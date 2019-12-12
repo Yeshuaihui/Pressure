e0ff609de49728bcee1abee9a88f3d49d7df7211
Yeshuaihui <357663494@qq.com>
2019-12-12 17:27:01 +0800

    Update HttpConfig.py

--- TestProject/Src/Config/HttpConfig.py
+++ TestProject/Src/Config/HttpConfig.py
@@ -1,6 +1,6 @@
 Config = {
     "OPPOProject": {
-        "baseHost": "http://apiceshi.8travelpay.com",#当前项目公共请求路径
+        "baseHost": "http://apiceshi.8travelpay.com",#当前项目公共请求路径 这个地址是我们公司的内网地址，有域名指向，使用时请修改
         "Header" : {},
         "cookie" : {}
     }
