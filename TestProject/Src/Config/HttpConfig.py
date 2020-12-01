f6dad06f90384b1aaba9306af65627538e6428d8
yeshuaihui <yeshuaihui@mallcoo.cn>
2020-12-01 18:42:42 +0800

    update

--- TestProject/Src/Config/HttpConfig.py
+++ TestProject/Src/Config/HttpConfig.py
@@ -1,8 +1,8 @@
 Config = {
-    "OPPOProject": {
+    "xicheng": {
         # 当前项目公共请求路径 这个地址是我们公司的内网地址，有域名指向，使用时请修改
-        "baseHost": "http://localhost:8091/",
-        "Header": {},
+        "baseHost": "http://mp-test.ygj.xincheng.com",
+        "Header": {"Authorization":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVc2VySUQiOjksIlVzZXJOYW1lIjoi6ZuG5Zui6LSm5Y-3NSIsIkFjY291bnQiOiJqdHpoNSIsIk1vYmlsZSI6IjE1MjA5MDExMjE1IiwiQWNjb3VudFR5cGUiOjEsIk1hbGxJZHMiOlsxNSwyOSw2Nl19.edalXB6sgsGI5CEF20Z0zCJkTIcutorW9KMjHlZoqvI"},
         "cookie": {}
     }
 
